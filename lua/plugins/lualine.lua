345455b19dbeb5718c2102ac6caf4cd9e181a23f
Anand Chandran <hello@achandran.com>
2023-02-24 15:57:11 -0500

    Update lualine time format

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+  -- Configure LazyVim to load catppuccin-frappe
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_z = {
+          function()
+            return " " .. os.date("%I:%M %p")
+          end,
+        },
+      },
+    },
+  },
+}
