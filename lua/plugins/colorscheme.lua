6b699117384a15dc48367015a4f374c40be6bcff
Anand Chandran <hello@achandran.com>
2023-03-01 09:33:10 -0500

    Use everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,14 @@
 return {
-  -- Configure LazyVim to load catppuccin-frappe
   {
-    "catppuccin/nvim",
+    "sainnhe/everforest",
     lazy = true,
     priority = 1000,
-    name = "catppuccin",
-    opts = {
-      custom_highlights = function(colors)
-        return {
-          DiffText = { bg = colors.yellow, fg = colors.base }, -- Make changed diff text more visible
-        }
-      end,
-    },
+    name = "everforest",
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "everforest",
     },
   },
 }
