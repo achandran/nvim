6b8d2bfd9aa88ee55cb16e3ed9521904ae49a1da
Anand Chandran <hello@achandran.com>
2023-04-14 09:01:53 -0400

    Use gruvbox light soft

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,18 @@
 return {
   {
-    "sainnhe/everforest",
+    "sainnhe/gruvbox-material",
     lazy = true,
     priority = 1000,
-    name = "everforest",
+    name = "gruvbox-material",
+    config = function()
+      vim.g.gruvbox_material_palette = "original"
+      vim.g.gruvbox_material_background = "soft"
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "gruvbox-material",
     },
   },
 }
